d608ad34bb0b201b636d1426c978c7a2c9bbf66c
Will Johnson <willster@willsterjohnson.com>
2022-05-10 09:33:04 +0100

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# JS-TS-DSA: JavaScript/TypeScript Data Structures and Algorithms
+
+## Development approach
+
+1. Determine the DSA to be implemented.
+2. Write a public interface for the baseline features class instance or function.
+3. Write a public interface for the additional features class instance or function.
+4. Write test cases based on the public interface.
+  i. Test the base functionality of the DSA
+  ii. Test the additional functionality of the DSA
+  iii. Test the DSA in a real use case
---
+++ test.sh
@@ -0,0 +1,19 @@
+#!usr/bin/bash
+
+arg1=$1;
+if [ -z $arg1 ]; then
+    arg1="*";
+    elif [ $arg1 = "-h" ] || [ $arg1 = "--help" ]; then
+    echo "Usage: pnpm test [test-pattern]";
+    echo "";
+    echo "Runs tests matching the given pattern, or all tests if no pattern specified.";
+    echo "Patterns only specify the file name, the \`.spec.ts\` extension is always added to the end of your pattern.";
+    exit;
+fi;
+
+# if arg1 != "*" echo "custom"
+if [ $arg1 != "*" ]; then
+    echo -e "\033[33mWARN: Using 'src/$arg1.spec.ts' as the test file pattern.\033[0m";
+    echo -e "\033[33m      This feature is haphazardly implemented and will not ensure the file(s) exist.\033[0m";
+fi;
+pnpm exec mocha "src/$arg1.spec.ts" --loader=ts-node/esm --experimental-specifier-resolution=node --timeout=30000 --recursive;
